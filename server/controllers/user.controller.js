4bcf2a2583b9a35decff6480f2ac77ff225f89a1
Divyam <divyamojas17@gmail.com>
2023-02-16 00:31:07 +0530

    Fix server bugs

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,25 @@
 import User from '../mongodb/models/user.js';
 
 const getAllUsers = async (req, res) => { }
-const createUser = async (req, res) => { }
+const createUser = async (req, res) => {
+    try {
+        const { name, email, avatar } = req.body;
+        const userExists = await User.findOne({ email });
+
+        if (userExists) return res.status(200).json(userExists);
+
+        const newUser = await User.create({
+            name,
+            email,
+            avatar
+        })
+
+        return res.status(200).json(newUser)
+    }
+    catch (error) {
+        res.status(500).json(error.message)
+    }
+}
 const getUserInfoById = async (req, res) => { }
 
 export {
