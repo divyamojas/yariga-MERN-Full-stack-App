c8a38de987ce5e177ae9e8cbad70cc2b3e2e3585
Divyam <divyamojas17@gmail.com>
2023-02-15 11:38:22 +0530

    Add Custom Button component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,7 +18,7 @@ import {
   VillaOutlined,
 } from "@mui/icons-material";
 import dataProvider from "@pankod/refine-simple-rest";
-import { MuiInferencer } from "@pankod/refine-inferencer/mui";
+// import { MuiInferencer } from "@pankod/refine-inferencer/mui";
 import routerProvider from "@pankod/refine-react-router-v6";
 import axios, { AxiosRequestConfig } from "axios";
 import { ColorModeContextProvider } from "contexts";
--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,7 +1,28 @@
+import { Add } from "@mui/icons-material";
+import { Box, Stack, Typography } from "@pankod/refine-mui";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import { CustomButton } from "components";
 import React from "react";
 
-const allProperties = () => {
-  return <div>allProperties</div>;
+const AllProperties = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          All Properties
+        </Typography>
+        <CustomButton
+          title="Add Property"
+          handleClick={() => {}}
+          backgroundColor="#475be8"
+          color="#fcfcfc"
+          icon={<Add />}
+        />
+      </Stack>
+    </Box>
+  );
 };
 
-export default allProperties;
+export default AllProperties;
